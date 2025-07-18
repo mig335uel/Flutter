eb6f26b8ca24fe2007624f85b2123553a8d30200
Miguel Angel <miguelangel.gomez.05@usal.es>
2025-07-18 19:28:33 +0200

    complete part of search of application

--- superhero_app/lib/Data/Model/superhero_response.dart
+++ superhero_app/lib/Data/Model/superhero_response.dart
@@ -1,12 +1,18 @@
+import 'package:superhero_app/Data/Model/superhero_details_response.dart';
+
 class SuperheroResponse{
 
   final String response;
+  final List<SuperheroDetailsResponse> result;
 
-
-  SuperheroResponse({required this.response});
+  SuperheroResponse({required this.response, required this.result});
 
   factory SuperheroResponse.fromJson(Map<String, dynamic> json){
-    return SuperheroResponse(response: json["response"]);
+    var list = json["results"] as List;
+    List<SuperheroDetailsResponse> listaHeroe = list.map((hero)=>SuperheroDetailsResponse.fromJson(hero)).toList();
+
+
+    return SuperheroResponse(response: json["response"], result: listaHeroe);
   }
 
 }
--- superhero_app/lib/View/super_hero_search.dart
+++ superhero_app/lib/View/super_hero_search.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:superhero_app/Data/Model/superhero_details_response.dart';
 import 'package:superhero_app/Data/Model/superhero_response.dart';
+import 'package:superhero_app/Data/repository.dart';
 
 class SuperHeroSearchScreen extends StatefulWidget {
   const SuperHeroSearchScreen({super.key});
@@ -9,7 +11,8 @@ class SuperHeroSearchScreen extends StatefulWidget {
 }
 
 class _SuperHeroSearchScreenState extends State<SuperHeroSearchScreen> {
-  late Future<SuperheroResponse?> _superHeroInfo;
+  Future<SuperheroResponse?>? _superHeroInfo;
+  Repository repo = Repository();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,17 +25,75 @@ class _SuperHeroSearchScreenState extends State<SuperHeroSearchScreen> {
               decoration: InputDecoration(
                 hintText: "Busca un superhéroe",
                 prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(48))
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(48),
+                ),
               ),
-              onChanged: (text){
-                  setState(() {
-                    
-                  });
+              onChanged: (text) {
+                setState(() {
+                  _superHeroInfo = repo.fetchSuperHeroInfo(text);
+                });
               },
             ),
+            bodyBuild(),
           ],
         ),
       ),
     );
   }
+
+  FutureBuilder bodyBuild() {
+    return FutureBuilder(
+      future: _superHeroInfo,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        } else if (snapshot.hasData) {
+          var listaHeroe = snapshot.data?.result;
+          return Expanded(
+            child: ListView.builder(
+              itemCount: listaHeroe?.length ?? 0,
+              itemBuilder: (context, index) {
+                if (listaHeroe != null) {
+                  return itemHero(listaHeroe[index]);
+                } else
+                  return null;
+              },
+            ),
+          );
+        } else {
+          return Text("No hay Resultados");
+        }
+      },
+    );
+  }
+
+  Padding itemHero(SuperheroDetailsResponse item) => Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black12),
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.red,
+    
+      ),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.network(
+              item.url,
+              width: double.infinity,
+              height: 250,
+              fit: BoxFit.cover,
+              alignment: Alignment(0, -0.6),
+            ),
+          ),
+          Text(item.name, style: TextStyle(fontSize: 20, color: Colors.white)),
+        ],
+      ),
+    ),
+  );
 }
