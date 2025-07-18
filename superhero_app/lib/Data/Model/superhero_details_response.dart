3f0c883182352bd430645d940bdec9265edbde53
Miguel Angel <miguelangel.gomez.05@usal.es>
2025-07-18 21:11:13 +0200

    include de powerStats and put in the details screen

--- superhero_app/lib/Data/Model/superhero_details_response.dart
+++ superhero_app/lib/Data/Model/superhero_details_response.dart
@@ -2,10 +2,52 @@ class SuperheroDetailsResponse {
   final String id;
   final String name;
   final String url;
+  final String? fullName;
+  final PowerStats powerStats;
+  SuperheroDetailsResponse({
+    required this.id,
+    required this.name,
+    required this.url,
+    required this.fullName,
+    required this.powerStats
+  });
 
-  SuperheroDetailsResponse({required this.id, required this.name, required this.url});
+  factory SuperheroDetailsResponse.fromJson(Map<String, dynamic> json) {
+    return SuperheroDetailsResponse(
+      id: json["id"],
+      name: json["name"],
+      url: json["image"]["url"],
+      fullName: json["biography"]["full-name"],
+      powerStats: PowerStats.fromJson(json["powerstats"])
+    );
+  }
+}
 
-  factory SuperheroDetailsResponse.fromJson(Map<String, dynamic> json){
-    return SuperheroDetailsResponse(id: json["id"], name: json["name"], url: json["image"]["url"]);
+class PowerStats {
+  final String intelligence;
+  final String strength;
+  final String speed;
+  final String durability;
+  final String power;
+  final String combat;
+
+  PowerStats({
+    required this.intelligence,
+    required this.strength,
+    required this.speed,
+    required this.durability,
+    required this.power,
+    required this.combat,
+  });
+
+  factory PowerStats.fromJson(Map<String, dynamic> json) {
+    return PowerStats(
+      intelligence: json["intelligence"],
+      strength: json["strength"],
+      speed: json["speed"],
+      durability: json["durability"],
+      power: json["power"],
+      combat: json["combat"],
+    );
   }
 }
--- superhero_app/lib/View/superhero_details_screen.dart
+++ superhero_app/lib/View/superhero_details_screen.dart
@@ -7,16 +7,72 @@ class SuperheroDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: topBar(),
-    );
-
+    return Scaffold(appBar: topBar(), body: bodyContent());
   }
-    AppBar topBar(){
-      return AppBar(
-        backgroundColor: Colors.red,
-        title: Text(superhero.name),
-        foregroundColor: Colors.white, 
-      );
-    }
+
+  AppBar topBar() {
+    return AppBar(
+      backgroundColor: Colors.red,
+      title: Text(superhero.name),
+      foregroundColor: Colors.white,
+    );
+  }
+
+  Column bodyContent() {
+    return Column(
+      children: [
+        Image.network(
+          superhero.url,
+          height: 300,
+          width: double.infinity,
+          fit: BoxFit.cover,
+          alignment: Alignment(0, -0.6),
+        ),
+        Text(
+          superhero.name,
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.indigo,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(61, 192, 192, 192),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: ListView(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Real Name: ${superhero.fullName}",
+                        style: TextStyle(fontSize: 15, color: Colors.indigo),
+                      ),
+                      Text("\n"),
+
+                      Text(
+                        "PowerStats:",
+                        style: TextStyle(fontSize: 20, color: Colors.indigo),
+                      ),
+                      Row(children: [
+                        Text("intelligence:\t", style: TextStyle(fontSize: 15, color: Colors.indigo)),
+                        Text(superhero.powerStats.intelligence, style: TextStyle(fontSize: 15, color: Colors.indigo)),
+                      ],)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
